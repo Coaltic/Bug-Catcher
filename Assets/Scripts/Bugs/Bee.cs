8dda2b27ed851ba649242f21c58bd5f577eac9d0
coaltic <glindsay156@gmail.com>
2026-09-21 13:30:22 -0300

    [ADDED] Bee is Now Catchable

---
+++ Assets/Scripts/Bugs/Bee.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Bee : Bug
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        bugName = "Bumble Bee";
+        rarity = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
